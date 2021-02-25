6ad4a2927c7c924d80f2929f2f722ddb3aab7987
phrofessor <78273987+phrofessor@users.noreply.github.com>
2021-02-24 19:47:18 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -93,4 +93,4 @@ def smtp_client(port = 1025, mailserver = "127.0.0.1"): #Fill in start #Fill in 
     clientSocket.close()
 
 if __name__ == '__main__':
-   smtp_client(1025, '127.0.0.1')
+    smtp_client(1025, '127.0.0.1')
